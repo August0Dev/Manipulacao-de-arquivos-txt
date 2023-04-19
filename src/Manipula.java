2b7989f1e2f089c0ba9787a2df58c149be8bddaa
Augusto <august.henriq@gmail.com>
2023-04-18 22:31:14 -0300

    Menu, repetição e leitor

--- src/Manipula.java
+++ src/Manipula.java
@@ -1,47 +1,61 @@
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Manipula {
-	public static void Write() {
+	static String endereco, nomeDoArquivo, texto;
+	static Scanner teclado = new Scanner(System.in);
+	static File arquivo;
+
+	public static void escreve() {
+
+		System.out.println("Digite o nome do arquivo: ");
+		nomeDoArquivo = teclado.nextLine() + ".txt";
+		System.out.println("Informe o endereço em que deseja criar o arquivo: ");
+		endereco = teclado.nextLine() + "\\" + nomeDoArquivo;
+
 		try {
-			System.out.println("Informe o endereço em que deseja criar o arquivo: ");
-			endereco = teclado.nextLine();
-			System.out.println("Digite o nome do arquivo: ");
-			nomeDoArquivo = teclado.nextLine() + ".txt";
-			
-			File arquivo = new File(endereco + "\\" + nomeDoArquivo); 
-			arquivo.createNewFile();															
-			
-			System.out.println("Arquivo criado com sucesso!");
-			
-			FileWriter arq = new FileWriter(endereco + "\\" + nomeDoArquivo); 
-			PrintWriter grava = new PrintWriter(arq);
+			arquivo = new File(endereco);
+			arquivo.createNewFile();
+
+			System.out.println("Arquivo criado com sucesso!\n");
+
+			PrintWriter grava = new PrintWriter(arquivo);
 			System.out.println("Insira um texto: ");
 			texto = teclado.nextLine();
 			grava.println(texto);
 			grava.close();
-			arq.close();
 			
-		}catch(IOException e) {
+			System.out.println("Texto inserido com sucesso!\n");
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void Read() {
-		
+
+	public static void le() throws IOException {
+		FileInputStream stream = new FileInputStream(endereco);
+		InputStreamReader reader = new InputStreamReader(stream);
+		BufferedReader br = new BufferedReader(reader);
+		String linha = br.readLine();
+		while (linha != null) {
+			System.out.println(linha);
+			linha = br.readLine();
+		}
 	}
-	
-	public static void Delete() {
-		System.out.println("Informe o endereço do arquivo: ");
-		endereco = teclado.nextLine();
-		System.out.println("Digite o nome do arquivo: ");
-		nomeDoArquivo = teclado.nextLine() + ".txt";
-		
+
+	public static void deleta() {
+
 		if (arquivo.delete()) {
-			System.out.println("Aquivo deletado.");
-		}else {
-			System.out.println("Arquivo não deletado.");
-	}
+			System.out.println("Aquivo deletado.\n");
+		} else {
+			System.out.println("Arquivo não deletado.\n");
+		}
+	}	
 }
+
+
--- src/Principal.java
+++ src/Principal.java
@@ -1,32 +1,40 @@
-import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Principal {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("MANIPULADOR DE .TXT\n");
-		
+
 		Scanner teclado = new Scanner(System.in);
-		String endereco, nomeDoArquivo, texto;
-		int opcao;
-		
-		System.out.println("1 - Inserir nome do arquivo");
-		System.out.println("2 - Exibir todos os nomes do arquivo;");
-		System.out.println("3 - apagar todo arquivo\n");
-		
-		System.out.println("Qual opção deseja selecionar?");
-		opcao = teclado.nextInt();
-		
-		if(opcao == 1) {
-			Manipula.Write();
-		}
-		else if(opcao == 2) {
-			Manipula.Read();
-		}
-		else if(opcao == 3) {
-			Manipula.Delete();
-		}
+
+		int opcao = 0;
+
+		do {
+			System.out.println("1 - Inserir nome do arquivo");
+			System.out.println("2 - Exibir todos os nomes do arquivo;");
+			System.out.println("3 - Apagar todo arquivo");
+			System.out.println("4 - Encerrar\n");
+
+			System.out.println("Qual opção deseja selecionar?");
+			opcao = teclado.nextInt();
+
+			switch (opcao) {
+			case 1:
+				Manipula.escreve();
+				break;
+			case 2:
+				Manipula.le();
+				break;
+			case 3:
+				Manipula.deleta();
+				break;
+			case 4:
+				break;
+			default:
+				System.out.println("A opção selecionada é inválida!");
+			}
+
+		} while (opcao != 4);
+
 	}
 }
